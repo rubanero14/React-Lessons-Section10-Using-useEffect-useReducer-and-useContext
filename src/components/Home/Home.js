4a94e8ed58d7cf8c9ed433dbed52d323ba5d4001
Rajaruban Rajindram <ahyat888@gmail.com>
2023-01-12 11:05:22 +0800

    added userName formatting logic for Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,9 +6,17 @@ import Button from "../UI/Button/Button";
 import classes from "./Home.module.css";
 
 const Home = (props) => {
+  const formattedUserName = props.userName
+    .split("@")[0]
+    .replaceAll(/[^\w|^\d]/gi, " ");
+
+  const name = formattedUserName.split(" ").map((name) => {
+    return name[0].toUpperCase() + name.slice(1).toLowerCase() + " ";
+  });
+
   return (
     <Card className={classes.home}>
-      <h1>Welcome back {props.userName}!</h1>
+      <h1>Welcome back {name}!</h1>
       <br />
       <Button style={{ textAlign: "center" }} onClick={props.onLogout}>
         Logout
